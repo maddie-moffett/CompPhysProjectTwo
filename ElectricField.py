69e0059042980a48ca7a029caaf2f13f4cb7f8fc
maddie.moffett <mnrmoffett@gmail.com>
2026-04-23 17:43:00 -0600

    part c done

--- ElectricField.py
+++ ElectricField.py
@@ -1,3 +1,13 @@
 import Relaxation
 import OverRelaxation
+from math import inf
 
+def PartC():
+    mini = inf
+    minnum = 0
+    for w in range(1.0, 1.6, 0.1):
+        nmin = OverRelaxation.OverRelaxation(w)
+        if nmin <= mini:
+            mini = nmin
+            minnum = w
+    return minnum
